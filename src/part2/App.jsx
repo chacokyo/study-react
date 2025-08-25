58cfdfb69d6e1e6baa2a332fcc181b331b804f36
chacokyo <3jisoo5@naver.com>
2025-08-25 13:51:45 +0900

    [add] 컴포넌트 import, export
    - 한 파일에서 하나의 default export, named export는 여러번 가능

---
+++ src/part2/App.jsx
@@ -0,0 +1,11 @@
+import Gallery from './Gallery'
+import { Profile } from './Profile'
+
+export default function Part2() {
+  return (
+    <>
+      <Profile />
+      <Gallery />
+    </>
+  )
+}
---
+++ src/part2/Profile.jsx
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <img
+      src="https://pethelpful.com/.image/w_1920,q_auto:good,c_fill,ar_4:3/MTczOTM3ODcwNjMxODcyMzUz/handle-hamster.jpg"
+      alt="hamster"
+    />
+  )
+}
