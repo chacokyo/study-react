d0bf5e8016a6969918312c19036075d843522911
chacokyo <3jisoo5@naver.com>
2025-08-25 17:19:45 +0900

    [add] children props를 사용한 JSX 전달

---
+++ src/part5/ex3/card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ title, children }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  )
+}
