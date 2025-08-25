15f5ba38d1cc477b1a6848281db9a3fd3010190a
chacokyo <3jisoo5@naver.com>
2025-08-25 18:00:32 +0900

    [add] if문으로 코드 리펙토링

---
+++ src/part6/ex3/Drink.jsx
@@ -0,0 +1,37 @@
+function Drink({ name }) {
+  let part, caffein, age
+
+  if (name === 'tea') {
+    part = 'leaf'
+    caffein = '15–70 mg/cup'
+    age = '4,000+ years'
+  } else if (name === 'coffee') {
+    part = 'bean'
+    caffein = '80–185 mg/cup'
+    age = '1,000+ years'
+  }
+
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{caffein}</dd>
+        <dt>Age</dt>
+        <dd>{age}</dd>
+      </dl>
+    </section>
+  )
+}
+
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+      <Drink name="hamster" />
+    </div>
+  )
+}
