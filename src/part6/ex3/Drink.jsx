41a0fecf06cf379f941c3a21401205248dfdcf7f
chacokyo <3jisoo5@naver.com>
2025-08-25 18:25:21 +0900

    [refactor] 객체를 이용 후 조건 제거

--- src/part6/ex3/Drink.jsx
+++ src/part6/ex3/Drink.jsx
@@ -1,26 +1,30 @@
-function Drink({ name }) {
-  let part, caffein, age
+const drinks = {
+  tea: {
+    part: 'leaf',
+    caffein: '15–70 mg/cup',
+    age: '4,000+ years',
+  },
+  coffee: {
+    part: 'bean',
+    caffein: '80–185 mg/cup',
+    age: '1,000+ years',
+  },
+}
 
-  if (name === 'tea') {
-    part = 'leaf'
-    caffein = '15–70 mg/cup'
-    age = '4,000+ years'
-  } else if (name === 'coffee') {
-    part = 'bean'
-    caffein = '80–185 mg/cup'
-    age = '1,000+ years'
-  }
+function Drink({ name }) {
+  const drink = drinks[name]
+  if (!drink) return <p>리스트에 없는 음료에요!</p>
 
   return (
     <section>
-      <h1>{name}</h1>
+      <h1 className="bg-amber-500">{name}</h1>
       <dl>
         <dt>Part of plant</dt>
-        <dd>{part}</dd>
+        <dd>{drink.part}</dd>
         <dt>Caffeine content</dt>
-        <dd>{caffein}</dd>
+        <dd>{drink.caffein}</dd>
         <dt>Age</dt>
-        <dd>{age}</dd>
+        <dd>{drink.age}</dd>
       </dl>
     </section>
   )
