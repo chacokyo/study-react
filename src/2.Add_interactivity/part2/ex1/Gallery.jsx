a5f1754cf38748a14cffd267ef7d5d93c269ecd0
chacokyo <3jisoo5@naver.com>
2025-08-27 23:53:30 +0900

    [add] 이벤트 핸들러내에 방어 조건 추가
    - 이벤트 핸들러 함수는 렌더링 되는 동안 선언된 모든 변수를 클로저로 참조

---
+++ src/2.Add_interactivity/part2/ex1/Gallery.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { sculptureList } from './data.js'
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
+
+  const hasPrev = index > 0
+  const hasNext = index < sculptureList.length - 1
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1)
+    }
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1)
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore)
+  }
+
+  const sculpture = sculptureList[index]
+  return (
+    <>
+      <button type="button" onClick={handlePrevClick} disabled={!hasPrev}>
+        Prev
+      </button>
+      <button type="button" onClick={handleNextClick} disabled={!hasNext}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  )
+}
