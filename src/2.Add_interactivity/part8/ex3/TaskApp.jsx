7cc0f46e5e7fc040dad1e86298b26f6f58cbc91d
chacokyo <3jisoo5@naver.com>
2025-08-29 10:23:22 +0900

    [add] todolist의 비변경 함수
    - handleAddTodo: 전개구문 활용
    - handleChangeTodo: map
    - handleDeleteTodo: filter

---
+++ src/2.Add_interactivity/part8/ex3/TaskApp.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import AddTodo from './AddTodo.jsx'
+import TaskList from './TaskList.jsx'
+
+let nextId = 3
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+]
+
+// 삭제 하려는
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos)
+
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false,
+      },
+    ])
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === nextTodo.id) {
+          return nextTodo
+        } else {
+          return todo
+        }
+      })
+    )
+  }
+
+  function handleDeleteTodo(nextTodoId) {
+    setTodos(todos.filter((todo) => todo.id !== nextTodoId))
+  }
+
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  )
+}
